676a3fc2f3133dcb38f0891454f746c203808dbb
danny thie <dannythie123@gmail.com>
2018-11-05 16:42:48 +0100

    bla

--- code/test2.py
+++ code/test2.py
@@ -1,1 +1,11 @@
-print('Hello world')
+width = int(input("How wide?"))
+height = int(input("How high?"))
+grid = []
+row = []
+bak = "."
+for i in range(width):
+    row.append('car')
+for i in range(height):
+    grid.append(row)
+for i in range(len(grid)):
+    print(grid[i])
