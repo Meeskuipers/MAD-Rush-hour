7c89639557bd9ca15ae43b77a9257e52a8cc4049
danny thie <dannythie123@gmail.com>
2018-11-12 15:48:32 +0100

    grid_class

--- code_2/grid.py
+++ code_2/grid.py
@@ -1,8 +1,15 @@
 class Grid():
     def __init__(self):
-        print('dit zou het moeten doen')
+        self.grid = self.load_grid()
 
-    def play(self):
-        print('deze ook')
-Main = Main()
-Main.play()
+    def load_grid(self):
+        gridsize = 6
+        grid = []
+        row = []
+        for i in range(gridsize):
+            row.append([])
+        for i in range(gridsize):
+            grid.append(row)
+        print(grid)
+Main = Grid()
+Main.load_grid()
