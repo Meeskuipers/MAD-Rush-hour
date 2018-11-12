a94559988484a9095c80d12edfb8865b57f6f9b9
Axel Huting <axhuting@xs4all.nl>
2018-11-12 11:52:32 +0100

    k

--- Code/prestigeprojectje.py
+++ Code/prestigeprojectje.py
@@ -7,48 +7,32 @@ import random
 allcars = [car1ver, car2ver, car3ver, car4ver, car1hor, car2hor, car3hor, car4hor, car5hor]
 
 #the empty cache used to evade loops and the set of possible moves
-cache = []
-possiblemoves = []
+
+
 
 #calculates possible moves
-def calculatemoves(grid,allcars):
-    freelist = freefunction(grid, allcars)
-
+def calculatemoves(grid, allcars):
+    freegrid = freefunction(grid,allcars)
+    movelist = []
     for i in allcars:
+        #move left
         if i[0][0] == i[1][0]:
-            trymoveright = i
-            trymoveright.pop(0)
-            updatevariable = trymoveright[0][1]
-            trymoveright[0][1] = updatevariable + 1
-            if trymoveright in freelist:
-                possiblemoves.append([i,'right'])
-
-            trymoveleft = i
-            trymoveleft.pop(1)
-            updatevariable = trymoveleft[0][1]
-            trymoveleft[0][1] = updatevariable - 1
-            if trymoveleft in freelist:
-                possiblemoves.append([i,'left'])
-
+            testleft = [i[0][0] ,i[0][1] - 1]
+            if testleft in freegrid:
+                movelist.append([i,'left'])
+            testright = [i[0][0],i[-1][1] +1]
+            if testright in freegrid:
+                movelist.append([i,'right'])
         if i[0][1] == i[1][1]:
-            trymovedown = i
-            trymovedown.pop(0)
-            updatevariable = trymovedown[0][0]
-            trymovedown.append(updatevariable+1)
+            testdown = [i[-1][0] + 1,i[1][1]]
+            if testdown in freegrid:
+                movelist.append([i,'down'])
+            testup = [i[0][0]-1,i[0][0]]
+            if testup in freegrid:
+                movelist.append([i,'up'])
+    return movelist
 
-            if trymovedown in freelist:
-                possiblemoves.append([i,'down'])
-
-            trymoveup = i
-            trymoveup.pop(1)
-            updatevariable = trymoveup[0][0]
-            trymoveup[0][0] = updatevariable - 1
-            if trymoveup in freelist:
-                possiblemoves.append([i,'up'])
-        return possiblemoves
-
-
-def move(grid, car):
+def move(car, move):
     newpos = []
     if move == 'left':
         for i in range(0,len(car)-1):
@@ -69,20 +53,43 @@ def move(grid, car):
 
 
 
+
+
+
 def solve(grid, allcars):
+    cache = []
     print('*calculating rushour*')
     print("i'LL BE BACK")
-    cache.append(grid)
     counter = 0
     while True:
+        freecoords = freefunction(grid,allcars)
+        cache.append(freecoords)
         possiblemoves = calculatemoves(grid,allcars)
+        if len(possiblemoves) == 0:
+            print('i failed')
+            break
         randomint = random.randint(0,len(possiblemoves)-1)
         chosenmove = possiblemoves[randomint]
         move(chosenmove[0],chosenmove[1])
+        freecoords = freefunction(grid,allcars)
+
+        #this does not work since it will loop around
+        if freecoords in cache:
+            if chosenmove[1] == 'left':
+                move(chosenmove[0],'right')
+            if chosenmove[1] == 'right':
+                move(chosenmove[0],'left')
+            if chosenmove[1] == 'up':
+                move(chosenmove[0],'down')
+            if chosenmove[1] == 'down':
+                move(chosenmove[0],'up')
         counter += 1
         if car2hor == [[3,5],[3,6]]:
-            print('you did it!')
-            print('it took you' + counter + 'moves')
+            print('it took')
+            print(counter)
+            print('moves to find john connor')
             break
 
-print(calculatemoves(grid,allcars))
+print(car5hor)
+move(car5hor,'left')
+print(car5hor)
