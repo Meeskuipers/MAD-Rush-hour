717dff9d70c279d24bb4100e2a9e61acd656a585
danny thie <dannythie123@gmail.com>
2018-11-16 12:21:50 +0100

    comments en net maken van main

--- code_2/main.py
+++ code_2/main.py
@@ -16,30 +16,25 @@ class Main():
         self.grid = [[0] * gridsize for i in range(gridsize)]
         return(self.grid)
 
-        
+    #Deze functie zet de informatie van een txt bestand om naar werkbare python formats
+    #Door middel van readline leest hij alle informatie uit de autos.txt bestand
     def load_cars(self, filename):
         with open(filename, "r") as file_cars:
             self.all_cars = []
             for line in file_cars:
-                position = []
                 line = line.strip()
                 if line.isdigit():
                     id = line.strip()
                     line = file_cars.readline()
                     direction = line.strip()
                     line = file_cars.readline().split()
-                    for coordinate in line:
-                        xy_list = []
-                        coordinate = coordinate.split(",")
-                        for xy in coordinate:
-                            xy_list.append(xy)
-                        position.append(xy_list)
-                    position = position
+                    position = [[xy for xy in coordinate.split(',')] for coordinate in line]
                     line = file_cars.readline()
                     type = line.strip()
                     auto = Auto(id, direction, position, type)
                     self.all_cars.append(auto)
 
+    #Deze functie voegt de ingeladen auto's van hierboven toe aan de grid
     def add_cars(self):
         for car in self.all_cars:
             Grid(self.grid, car.id, car.position)
@@ -48,17 +43,18 @@ class Main():
     def won(self):
         if self.grid[2][5] == '6':
             return(True)
-        else:
-            return(False)
 
     def play(self):
         print("Heee let's play!!!")
+        #Deze for loop print de grid op een elegante manier.
         for row in self.grid:
             for number in row:
                 print("", end=" ")
                 print(number, end=" ")
             print()
-        while not self.won():
+            #Door deze while loop blijft de grid uitgeprint worden nadat er een move is gedaan
+            #Hij stopt pas als de 'won' functie true returnt
+            while not self.won():
             command = input("> ").upper().split(',')
             move = self.move(command)
             if not move:
@@ -73,7 +69,8 @@ class Main():
                         print(number, end=" ")
                     print()
         print("You won!!!")
-
+    #Deze functie wordt aangeroepen om een move te maken.
+    #Een move heeft 2 inputs nodig: de direction en car_id   
     def move(self, command):
         if len(command) != 2:
             print("huh")
