0c1a0b8602bf990c0606044b4ea03363621aa7a1
Axel Huting <axhuting@xs4all.nl>
2018-11-16 12:09:38 +0100

    car class is nu mooi

--- code_2/class_auto.py
+++ code_2/class_auto.py
@@ -1,4 +1,6 @@
 
+#de class auto definieert autos binnen onze grid. iedere auto heeft een id nummer,
+#direction, positie en een type
 class Auto(object):
     def __init__(self, id, direction, position, type):
         self.id = id
@@ -6,45 +8,24 @@ class Auto(object):
         self.position = position
         self.type =  type
 
+#move_car is redelijk self explanatory. move_car geeft de gespecificeerde auto
+#een andere positie afhankelijk van de gespecificeerde command.
     def move_car(self, direction, grid):
+        #beweegt de auto naar links
         if direction == 'LEFT':
-            if self.direction == "VERTICAAL":
-                return False
-            else:
-                self.position = [[elem[0],int(elem[1])-1] for elem in self.position]
-                if self.check_move(direction, grid, self.position):
-                    return True
-                else:
-                    return False
+            self.position = [[elem[0],int(elem[1])-1] for elem in self.position]
+
+        #beweegt de auto naar rechts
         elif direction == 'RIGHT':
-            if self.direction == "VERTICAAL":
-                return False
-            else:
-                self.position = [[elem[0],int(elem[1])+1] for elem in self.position]
-                return True
+            self.position = [[elem[0],int(elem[1])+1] for elem in self.position]
 
+        #beweegt de auto naar boven
         elif direction == 'UP':
-            if self.direction == "HORIZONTAAL":
-                return False
-            else:
-                self.position = [[int(elem[0])-1,elem[1]] for elem in self.position]
-                return True
+            self.position = [[int(elem[0])-1,elem[1]] for elem in self.position]
 
+        #beweegt de auto naar beneden
         elif direction == 'DOWN':
-            if self.direction == "HORIZONTAAL":
-                return False
-            else:
-                self.position = [[int(elem[0])+1,elem[1]] for elem in self.position]
-                return True
-        else:
-            return False
-
-    def check_move(self, direction, grid, position):
-        if direction == 'LEFT':
-            if grid[int(position[0][0])][int(position[0][1])] == 0:
-                return(True)
-            else:
-                return(False)
-
+            self.position = [[int(elem[0])+1,elem[1]] for elem in self.position]
+            
     def __str__(self):
         return f"{self.id}\n{self.position}\n{self.direction}{self.type}\n"
