706eccca548426892375e2413577f2963ba03016
danny thie <dannythie123@gmail.com>
2018-11-27 14:57:13 +0100

    hij doet het wel hoor axel

--- code/main.py
+++ code/main.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-from dumbsolver import dumbsolver
+
+#from dumbsolver import dumbsolver
 #from informed import informed
-=======
 from algoritmes.dumbsolver import dumbsolver
->>>>>>> 35bcb0ca61521535395ddea277e61f078f5cb9cc
+
 from play import play
 
 def main():
