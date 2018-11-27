0b308b6cc7345808880ba8c4f5808e386e9f3721
Mees Kuipers <mees.kuipers1998@gmail.com>
2018-11-27 08:43:33 +0100

    update

--- code/dumbsolver.py
+++ code/dumbsolver.py
@@ -15,7 +15,7 @@ def dumbsolver(size):
         move(grid,[possible_moves[randommove][0],possible_moves[randommove][1]])
         counter += 1
         grid.grid = grid.update()
-        show_grid(grid.grid)
+        # show_grid(grid.grid)
 
     print("it took "+ " " +str(counter)+ " " + " moves to win (for the computer, you're an idiot who chose solve)")
 
@@ -23,10 +23,10 @@ def won(grid):
     if grid[2][5] == 6:
         return True
 
-def show_grid(grid):
-    for i in grid:
-        for x in i:
-            print("  ", end='')
-            print(x, end='')
-        print("")
-    print("")
+# def show_grid(grid):
+#     for i in grid:
+#         for x in i:
+#             print("  ", end='')
+#             print(x, end='')
+#         print("")
+#     print("")
