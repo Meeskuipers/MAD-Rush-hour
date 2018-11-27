8e7c91626f6684b1a3980918b5670453718cc3bb
danny thie <dannythie123@gmail.com>
2018-11-27 13:42:31 +0100

    rip dijkstra

--- code/main.py
+++ code/main.py
@@ -6,13 +6,13 @@ def main():
     ''' docstring placeholder
     '''
     gridsize = 6
-    choice = input('type play or dumbsolve    ')
+    choice = input('type play or dumbsolve or informed    ')
     if choice == 'play':
         play(int(gridsize))
     elif choice == 'dumbsolve':
         dumbsolver(int(gridsize))
-    elif choice == 'dijkstra':
-        dijkstra(int(gridsize))
+    elif choice == 'informed':
+        informed(int(gridsize))
     else:
         print('Thats is not a valid input')
         main()
