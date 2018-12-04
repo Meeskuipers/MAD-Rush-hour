cd136815f5b749310c502318bc39afe9afb2d63e
Axel Huting <axhuting@xs4all.nl>
2018-12-04 16:50:58 +0100

    voor angelo

--- code/helper/checkwin.py
+++ code/helper/checkwin.py
@@ -1,13 +1,13 @@
 from math import ceil
 
 def win(grid, size):
-    if grid[2][5] == 6:
-        return True
-    else:
-        return False
-    # victoryx = ceil(size/2) -1
-    # victoryy = size - 1
-    # if grid[victoryx][victoryy] == 6:
+    # if grid[2][5] == 6:
     #     return True
     # else:
     #     return False
+    victoryx = ceil(size/2) -1
+    victoryy = size - 1
+    if grid[victoryx][victoryy] == 6:
+        return False
+    else:
+        return True
