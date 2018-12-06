b33ad5b8d4ccb647b3ea29cecf0f548c8a0051c7
crumbleyo <44553946+crumbleyo@users.noreply.github.com>
2018-12-06 14:23:48 +0100

    Update dumbsolver.py

--- code/algoritmes/dumbsolver.py
+++ code/algoritmes/dumbsolver.py
@@ -22,11 +22,7 @@ def dumbsolver(size,bord):
             dumbsolver(size,bord)
     if counter < 1999:
         print("it took "+ " " +str(counter)+ " " + " moves to win (for the computer, you're an idiot who chose solve)")
-<<<<<<< HEAD
         return answer
-=======
-        return counter
->>>>>>> aaea8153b8d44a9e3f41395c942118cead116209
 
 def won(grid):
     if grid[2][5] == 6:
